7901a509411df01cce824c79f2cb3faaec1fff42
nrupali.thakur <nrupali.thakur@springct.com>
2024-10-12 18:40:35 +0530

    change

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 
-let html = fs.readFileSync('./test.html', 'utf8');
-let browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-});
+async function run() {
+    let html = fs.readFileSync('./test.html', 'utf8');
+    let browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
 
-const pdfOptions = {
-    path: './index.pdf',
-    format: 'A4',
-    printBackground: true,
-    // Optionally set dimensions if needed
-    width: '210mm',
-    height: '297mm',
+    const pdfOptions = {
+        path: './index.pdf',
+        format: 'A4',
+        printBackground: true,
+        // Optionally set dimensions if needed
+        width: '210mm',
+        height: '297mm',
 
-};
+    };
 
-let page = await browser.newPage();
-await page.setContent(html);
-await page.pdf(pdfOptions);
-await browser.close();
+    let page = await browser.newPage();
+    await page.setContent(html);
+    await page.pdf(pdfOptions);
+    await browser.close();
+}
+
+run();
